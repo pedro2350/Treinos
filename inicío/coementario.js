83e7fd055eae100a93517aed59357c8d265aaee0
pedro2350 <pedrohenrique70866gmail.com>
2022-07-10 12:42:45 -0300

    two commit

---
+++ inicío/coementario.js
@@ -0,0 +1,9 @@
+//console.log é usado paara exiobir algo na tela do pc!
+console.log("bom dia");
+console.log("boa tarde");
+console.log("boa noite");
+
+/*
+Esse é um exemplo de comentario
+com multiplas linhas!s
+*/
---
+++ inicío/sentenca_1.js
@@ -0,0 +1,5 @@
+console.
+         log("bom dia");
+         console.log("boa tarde");
+         console.log("boa noite");
+         
